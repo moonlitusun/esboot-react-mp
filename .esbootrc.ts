3eda014e88b05b7fdffc7e0b63a571d3ed7aad45
Roc <moonlitusun@gmail.com>
2026-07-02 17:17:52 +0800

    feat: upgrade esboot

--- .esbootrc.ts
+++ .esbootrc.ts
@@ -2,23 +2,12 @@ import type { UserOptions } from '@dz-web/esboot';
 import type { BundlerRspackOptions } from '@dz-web/esboot-bundler-rspack';
 import type { BundlerViteOptions } from '@dz-web/esboot-bundler-vite';
 import process from 'node:process';
-import { defineConfig, definePlugin, PluginHooks } from '@dz-web/esboot';
-import {
-  BundlerRspack,
-  CodeSplittingType as CodeSplittingTypeRspack,
-} from '@dz-web/esboot-bundler-rspack';
+import { defineConfig, entryLogPlugin } from '@dz-web/esboot';
+import { BundlerRspack } from '@dz-web/esboot-bundler-rspack';
 import { BundlerVite, CodeSplittingType as CodeSplittingTypeVite } from '@dz-web/esboot-bundler-vite';
 import vitestPlugin from '@dz-web/esboot-plugin-vitest';
 
 const PX2REM_EXCLUDE = [/node_modules/];
-const EXTRA_BABEL_INCLUDES = [
-  /zustand/i,
-  /query-string/i,
-  /react-intl/i,
-  /@tanstack/i,
-  /@floating-ui/i,
-  /tailwind-merge/i,
-];
 
 export default defineConfig<BundlerRspackOptions | BundlerViteOptions>(cfg => ({
   ...(process.env.ESBOOT_BUNDLER === 'rspack' ? getBundlerRspackOptions() : getBundlerViteOptions()),
@@ -28,22 +17,9 @@ export default defineConfig<BundlerRspackOptions | BundlerViteOptions>(cfg => ({
     rootValue: cfg.isMobile ? 32 : 16,
     exclude: PX2REM_EXCLUDE,
   },
-  define: {
-    'process.env.isMobile': cfg.isMobile,
-    'process.env.isBrowser': cfg.isBrowser,
-  },
   plugins: [
     vitestPlugin(),
-    definePlugin({
-      key: 'log',
-      [PluginHooks.afterCompile]: (cfg) => {
-        const { isDev } = cfg;
-        if (!isDev)
-          return;
-
-        console.log(cfg.entry);
-      },
-    }),
+    entryLogPlugin(),
   ],
 }));
 
@@ -59,20 +35,27 @@ const frameworkBundles = [
   'react-intl',
 ];
 
+const codeSplitting = {
+  jsStrategy: CodeSplittingTypeVite.granularChunks,
+  jsStrategyOptions: {
+    frameworkBundles,
+  },
+};
+
+const EXTRA_BABEL_INCLUDES = [
+  /zustand/i,
+  /query-string/i,
+  /react-intl/i,
+  /@tanstack/i,
+  /@floating-ui/i,
+  /tailwind-merge/i,
+];
+
 function getBundlerViteOptions(): UserOptions<BundlerViteOptions> {
   return {
     bundler: BundlerVite,
     bundlerOptions: {
-      customConfig: (viteConfig) => {
-        viteConfig.optimizeDeps = { exclude: ['@dz-web/bridge', '@dz-web/bridge-core', '@dz-web/bridge-mock'] };
-        return viteConfig;
-      },
-      codeSplitting: {
-        jsStrategy: CodeSplittingTypeVite.granularChunks,
-        jsStrategyOptions: {
-          frameworkBundles,
-        },
-      },
+      codeSplitting,
     },
   };
 }
@@ -82,12 +65,7 @@ function getBundlerRspackOptions(): UserOptions<BundlerRspackOptions> {
     bundler: BundlerRspack,
     bundlerOptions: {
       extraBabelIncludes: EXTRA_BABEL_INCLUDES,
-      codeSplitting: {
-        jsStrategy: CodeSplittingTypeRspack.granularChunks,
-        jsStrategyOptions: {
-          frameworkBundles,
-        },
-      },
+      codeSplitting,
     },
   };
 }
